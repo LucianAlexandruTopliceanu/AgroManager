71b85a6aa5dea87d41290b9404ddd521593e49eb
Lucian Alexandru Topliceanu <alexandru.lucian.topliceanu@gmail.com>
2025-10-07 15:44:33 +0200

    Aggiunta al Domain Model StatoPiantagione (Ha 5 stati pre inseriti nel db)

---
+++ src/main/java/DomainModel/StatoPiantagione.java
@@ -0,0 +1,105 @@
+package DomainModel;
+
+import java.time.LocalDateTime;
+
+
+public class StatoPiantagione {
+    private Integer id;
+    private String codice;
+    private String descrizione;
+    private LocalDateTime dataCreazione;
+    private LocalDateTime dataAggiornamento;
+
+
+    public StatoPiantagione() {}
+
+    public StatoPiantagione(Integer id, String codice, String descrizione) {
+        this.id = id;
+        this.codice = codice;
+        this.descrizione = descrizione;
+    }
+
+    public StatoPiantagione(String codice, String descrizione) {
+        this.codice = codice;
+        this.descrizione = descrizione;
+    }
+
+
+    public static final String ATTIVA = "ATTIVA";
+    public static final String RIMOSSA = "RIMOSSA";
+    public static final String IN_RACCOLTA = "IN_RACCOLTA";
+    public static final String COMPLETATA = "COMPLETATA";
+    public static final String SOSPESA = "SOSPESA";
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCodice() {
+        return codice;
+    }
+
+    public void setCodice(String codice) {
+        this.codice = codice;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public LocalDateTime getDataCreazione() {
+        return dataCreazione;
+    }
+
+    public void setDataCreazione(LocalDateTime dataCreazione) {
+        this.dataCreazione = dataCreazione;
+    }
+
+    public LocalDateTime getDataAggiornamento() {
+        return dataAggiornamento;
+    }
+
+    public void setDataAggiornamento(LocalDateTime dataAggiornamento) {
+        this.dataAggiornamento = dataAggiornamento;
+    }
+
+
+    public boolean isAttiva() {
+        return ATTIVA.equals(codice);
+    }
+
+    public boolean isRimossa() {
+        return RIMOSSA.equals(codice);
+    }
+
+    public boolean isCompletata() {
+        return COMPLETATA.equals(codice);
+    }
+
+    @Override
+    public String toString() {
+        return descrizione;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        StatoPiantagione that = (StatoPiantagione) obj;
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
