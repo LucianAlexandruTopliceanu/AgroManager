5c9f4a68f03b11414712f7ef28cf10024ea50fd2
Lucian Alexandru Topliceanu <alexandru.lucian.topliceanu@gmail.com>
2025-10-13 10:08:20 +0200

    Modifica strategy per migliore implementazione MVC e separazione responsabilita

--- src/main/java/BusinessLogic/Strategy/ProcessingResult.java
+++ src/main/java/BusinessLogic/Strategy/ProcessingResult.java
@@ -3,18 +3,12 @@ package BusinessLogic.Strategy;
 
 public class ProcessingResult<T> {
     private final T value;
-    private final String formattedOutput;
 
-    public ProcessingResult(T value, String formattedOutput) {
+    public ProcessingResult(T value) {
         this.value = value;
-        this.formattedOutput = formattedOutput;
     }
 
     public T getValue() {
         return value;
-    }
-
-    public String getFormattedOutput() {
-        return formattedOutput;
     }
 }
